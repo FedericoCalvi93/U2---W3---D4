7c507695353af81f98d19e2cdc13a6f9a19d4b6a
FedericoCalvi93 <federicocalvi.93@gmail.com>
2023-07-06 17:00:00 +0200

    first commit

---
+++ template.js
@@ -0,0 +1,44 @@
+document.getElementById("hideButton1").addEventListener("click", function() {
+    const card = document.getElementById("card1");
+    card.style.display = "none";
+  });
+
+  document.getElementById("hideButton2").addEventListener("click", function() {
+    const card = document.getElementById("card2");
+    card.style.display = "none";
+  });
+
+  document.getElementById("hideButton3").addEventListener("click", function() {
+    const card = document.getElementById("card3");
+    card.style.display = "none";
+  });
+
+  document.getElementById("hideButton4").addEventListener("click", function() {
+    const card = document.getElementById("card4");
+    card.style.display = "none";
+  });
+
+  document.getElementById("hideButton5").addEventListener("click", function() {
+    const card = document.getElementById("card5");
+    card.style.display = "none";
+  });
+
+  document.getElementById("hideButton6").addEventListener("click", function() {
+    const card = document.getElementById("card6");
+    card.style.display = "none";
+  });
+
+  document.getElementById("hideButton7").addEventListener("click", function() {
+    const card = document.getElementById("card7");
+    card.style.display = "none";
+  });
+  
+  document.getElementById("hideButton8").addEventListener("click", function() {
+    const card = document.getElementById("card8");
+    card.style.display = "none";
+  });
+
+  document.getElementById("hideButton9").addEventListener("click", function() {
+    const card = document.getElementById("card9");
+    card.style.display = "none";
+  });
